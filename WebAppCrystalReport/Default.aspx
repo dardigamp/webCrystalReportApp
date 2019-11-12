43926d752b11b00792b83d4c23aab9a9cb4221d9
Darwin <Darwin@DARWIN-PC>
2019-11-11 22:40:37 -0500

    Agregar archivos de proyecto.

---
+++ WebAppCrystalReport/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebAppCrystalReport.Default" %>
+
+<%@ Register assembly="CrystalDecisions.Web, Version=13.0.3500.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" namespace="CrystalDecisions.Web" tagprefix="CR" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <br />
+        <CR:CrystalReportViewer ID="CrystalReportViewerAlumno" runat="server" AutoDataBind="true" />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebAppCrystalReport/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppCrystalReport
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if (Page.IsPostBack)
+            //{
+                BDUTPEntities1 mde = new BDUTPEntities1();
+                CrystalReportAlumno crp = new CrystalReportAlumno();
+                crp.SetDataSource(mde.Alumnoes.Select(a => new {
+                    Codigo = a.Codigo,
+                    Apellidos = a.Apellidos,
+                    Nombres = a.Nombres,
+                    Documento = a.Documento,
+                    Telefono = a.Telefono
+                }));
+                this.CrystalReportViewerAlumno.ReportSource = crp;
+                this.CrystalReportViewerAlumno.DataBind();
+            //}
+        }
+    }
+}
